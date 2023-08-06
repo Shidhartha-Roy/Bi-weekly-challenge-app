e185a64582c5304dc53aa10c9f1cd0064c46bdbe
Shidhartha <sidroy192@gmail.com>
2023-08-06 18:23:12 +0530

    Initial Commit Creation

---
+++ bi-weekly-api/src/main/java/com/biweeklychallange/biweeklyapi/BiWeeklyApiApplication.java
@@ -0,0 +1,13 @@
+package com.biweeklychallange.biweeklyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiWeeklyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiWeeklyApiApplication.class, args);
+	}
+
+}
---
+++ bi-weekly-api/src/test/java/com/biweeklychallange/biweeklyapi/BiWeeklyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biweeklychallange.biweeklyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiWeeklyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
