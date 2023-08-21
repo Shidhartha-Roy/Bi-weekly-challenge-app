8c13a07cd0a4b69e494b5162084e8e8bf6f181d0
Shidhartha <sidroy192@gmail.com>
2023-08-22 04:38:37 +0530

    Ordering of Challenges

--- bi-weekly-api/src/main/java/com/biweeklychallange/biweeklyapi/repository/ChallengeRepository.java
+++ bi-weekly-api/src/main/java/com/biweeklychallange/biweeklyapi/repository/ChallengeRepository.java
@@ -2,12 +2,13 @@ package com.biweeklychallange.biweeklyapi.repository;
 
 import com.biweeklychallange.biweeklyapi.entity.ChallengeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ChallengeRepository extends JpaRepository<ChallengeEntity, Long> {
-
+    @Query("SELECT c FROM ChallengeEntity c ORDER BY endDate")
     List<ChallengeEntity> findAllByUsername(String username);
 }
